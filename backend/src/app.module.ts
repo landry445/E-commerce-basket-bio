8ae343f99c3926bb70aeec65f488c39f5a5317d6
Landry Dupont <landrydupont790@gmail.com>
2025-06-26 11:29:26 +0200

    first commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BasketsModule } from './baskets/baskets.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { PickupModule } from './pickup/pickup.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: false, // à mettre à true uniquement pour du dev local !
+    }),
+    UsersModule,
+    BasketsModule,
+    ReservationsModule,
+    PickupModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+import forms from '@tailwindcss/forms';
+
+const tailwindConfig = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  plugins: [forms],
+};
+
+export default tailwindConfig;
+
