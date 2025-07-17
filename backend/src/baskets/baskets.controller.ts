59ee85d5ec2cfd8295841f59b47b7701bd8fe8c1
Landry Dupont <landrydupont790@gmail.com>
2025-07-17 09:59:35 +0200

    update auth-endpoints

--- backend/src/baskets/baskets.controller.ts
+++ backend/src/baskets/baskets.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get, Post, Body, Param, Put, Delete, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  ParseUUIDPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateBasketDto } from './dto/create-basket.dto';
 import { BasketsService } from './baskets.service';
 import { Basket } from './entities/basket.entity';
+import { JwtAuthGuard } from '../auth/strategies/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { Role } from 'src/auth/role.enum';
 
 @Controller('baskets')
 export class BasketsController {
   constructor(private readonly basketsService: BasketsService) {}
 
+  // Public - tout le monde peut voir les paniers
   @Get()
   findAll(): Promise<Basket[]> {
     return this.basketsService.findAll();
@@ -17,16 +32,23 @@ export class BasketsController {
     return this.basketsService.findOne(id);
   }
 
+  // ---- Les routes suivantes sont réservées à l'admin ----
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Post()
   create(@Body() dto: CreateBasketDto): Promise<Basket> {
     return this.basketsService.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Put(':id')
   update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: CreateBasketDto): Promise<Basket> {
     return this.basketsService.update(id, dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
     return this.basketsService.remove(id);
